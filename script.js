bd9f0cb52c720df40cd7291017ffd669487987a3
alaa <alaa1190a@gmail.com>
2026-02-26 12:11:34 +0200

    first section done.

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 //1 
+// print into the conscle
+/*
+print
+hello
+
+*/
 console.log("Hello JavaScript World");
+
+//3
+console.log("alaa alawneh // 22 years old // palestine");
+
+//4
+console.log("5" + 5); // 55 will be printed
+
+//5
+console.log(10 + 20 + "30"); // first operation is arthmitic second is concationation.
+
+
